80bccced153d1c6d3f061573177043fafaeb7894
Giuulob89 <giulialobo89@gmail.com>
2020-10-17 23:24:30 -0300

    First Page ready

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,23 @@ function App() {
   return (
     <div id = "page-landing">
       <div className="content-wrapper">
+        
         <img src={logoImg} alt="Happy"/>
+        
         <main>
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos  e mude o dia  de muitas crianças.</p>
         </main>
+        
         <div className="location">
           <strong>Brasília</strong>
           <strong>Distrito Federal</strong>
         </div>
+        
         <a href="" className="enter-app">
             <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
         </a>
+      
       </div>
     </div>
   );
